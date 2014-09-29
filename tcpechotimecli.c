9e9496fe6d40721fb813d945ef757ab0e4cdfd03
Ankur Mittal <ankurmit2006@gmail.com>
2014-09-28 20:50:45 -0400

    able to fork child and setup pipe.

--- tcpechotimecli.c
+++ tcpechotimecli.c
@@ -1,16 +1,56 @@
 #include "unp.h"
+#include <stdio.h>
 
 static struct hostent* gethostinfo(char *target, struct sockaddr_in servaddr);
 
+static void process_service(int choice, struct hostent* hostp)
+{	
+
+	pid_t childpid;
+	int read_write_s;
+	int pfd[2];
+	char buf[19];
+	if (pipe(pfd) == -1)
+	{
+		perror("Pipe failed");
+		exit(1);
+	}
+	childpid = fork();
+	if(childpid >= 0)
+	{
+		if(childpid == 0) //Child Process
+		{
+			close(pfd[0]);
+			read_write_s = write(pfd[1], "message from child", 19);
+			close(pfd[1]);
+			exit(0);
+		}
+		else // Parent Process 
+		{
+			close(pfd[1]);
+			while ((read_write_s = read(pfd[0], buf, 19))
+					!= 0)
+				printf("child read %s\n", buf);
+			close(pfd[0]);
+		}
+
+	}
+	else
+	{
+		perror("Forking failed!!!. Please try again.");
+	}
+}
+
 int main(int argc, char **argv)
 {
-	int sockfd, n;
+	int sockfd, choice;
 	socklen_t len;
 	struct sockaddr_in servaddr;
 	struct hostent *hostp;
+	char term;
 
 	if (argc != 2)
-		err_quit("usage: client <IPaddress or hostname>");
+		err_quit("usage: client <IP Address or hostname>");
 
 	if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 		err_sys("socket error");
@@ -19,12 +59,39 @@ int main(int argc, char **argv)
 	servaddr.sin_family = AF_INET;
 	//servaddr.sin_port   = htons(13);        /* daytime server */
 	hostp = gethostinfo(argv[1], servaddr);
+	close(sockfd);
 	if(hostp == NULL) 
 	{
-	    close(sockfd);
-	    exit(-1);
+		exit(-1);
 	}
+
+	do {
+		printf("\n1. Echo\n");
+		printf("2. Time\n");
+		printf("3. Exit\n");
+		printf("Please choose a option: ");
+		if(scanf("%d%c", &choice, &term) != 2 || term != '\n')
+		{
+			choice = 0;
+			do {
+				term = fgetc(stdin);
+			} while (term != '\n' && term != EOF);
+			printf("Invalid Input. Please try again.");
+		}
+		else
+		{
+			switch(choice)
+			{
+				case 1: 
+				case 2: process_service(choice, hostp);
+						break;
+				case 3: break;
+				default: printf("Invalid Input. Please try again.");
+			}
+		}	
+	} while(choice != 3);
 }
+
 
 
 struct hostent* gethostinfo(char *target, struct sockaddr_in servaddr)
@@ -55,8 +122,6 @@ struct hostent* gethostinfo(char *target, struct sockaddr_in servaddr)
 			return NULL;
 		}
 		printf("Hostname is: %s\n", hostp->h_name);
-
-
 	}
 	return hostp;
 
